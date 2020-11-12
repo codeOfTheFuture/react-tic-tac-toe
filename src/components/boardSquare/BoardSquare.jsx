6a419fcfb3cd766162577c660cdffdbed7ea0708
Jeff Oliver <jeffoliver3384@gmail.com>
2020-11-12 18:16:58 -0500

    game over displays game winners name or draw

--- src/components/boardSquare/BoardSquare.jsx
+++ src/components/boardSquare/BoardSquare.jsx
@@ -15,6 +15,7 @@ const BoardSquare = ({ boardSquareNum }) => {
   const players = useSelector(({ players }) => players);
   const currentPlayer = useSelector(({ game }) => game.currentPlayer);
   const totalMoves = useSelector(({ game }) => game.totalMoves);
+  const gameEnded = useSelector(({ game }) => game.gameOver);
 
   const dispatch = useDispatch();
 
@@ -22,30 +23,32 @@ const BoardSquare = ({ boardSquareNum }) => {
   const [playerSymbol, setPlayerSymbol] = useState("");
 
   const selectBoardSquare = () => {
-    if (boardSquareSelected === false) {
-      setBoardSquareSelected(true);
-      setPlayerSymbol(currentPlayer.symbol);
+    if (!gameEnded) {
+      if (boardSquareSelected === false) {
+        setBoardSquareSelected(true);
+        setPlayerSymbol(currentPlayer.symbol);
 
-      let updatedPlayer;
-      updatedPlayer = setPlayerMoves(currentPlayer, boardSquareNum);
-      console.log(updatedPlayer);
-      updatedPlayer = checkPlayerMoves(updatedPlayer);
+        let updatedPlayer;
+        updatedPlayer = setPlayerMoves(currentPlayer, boardSquareNum);
+        console.log(updatedPlayer);
+        updatedPlayer = checkPlayerMoves(updatedPlayer);
 
-      if (totalMoves + 1 === 9) {
-        dispatch(gameOver());
-      } else if (updatedPlayer.wonGame) {
-        dispatch(gameOver());
-      } else {
-        dispatch(updateTotalMoves(totalMoves + 1));
-        dispatch(updatePlayer(updatedPlayer));
+        if (totalMoves + 1 === 9) {
+          dispatch(gameOver());
+        } else if (updatedPlayer.wonGame) {
+          dispatch(gameOver());
+        } else {
+          dispatch(updateTotalMoves(totalMoves + 1));
+          dispatch(updatePlayer(updatedPlayer));
+
+          if (currentPlayer.symbol === "X") {
+            dispatch(setCurrentPlayer(players.player2));
+          }
+          if (currentPlayer.symbol === "O") {
+            dispatch(setCurrentPlayer(players.player1));
+          }
+        }
       }
-    }
-
-    if (currentPlayer.symbol === "X") {
-      dispatch(setCurrentPlayer(players.player2));
-    }
-    if (currentPlayer.symbol === "O") {
-      dispatch(setCurrentPlayer(players.player1));
     }
   };
 
--- src/components/gameOver/GameOver.jsx
+++ src/components/gameOver/GameOver.jsx
@@ -3,11 +3,17 @@ import { useSelector, useDispatch } from "react-redux";
 
 const GameOver = () => {
   const gameOver = useSelector(({ game }) => game.gameOver);
+  const currentPlayer = useSelector(({ game }) => game.currentPlayer);
+
+  const congrats = <h4>Congrats {currentPlayer.name}! You Won</h4>;
+
+  const draw = <h4>Draw</h4>;
 
   return (
     gameOver && (
       <div data-test='component-game-over'>
         <h3>Game Over</h3>
+        {currentPlayer.wonGame ? congrats : draw}
         <button>New Game</button>
       </div>
     )
